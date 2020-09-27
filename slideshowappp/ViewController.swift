5493ec6520deba348c87b69a2c1eccc2021ba9b1
jintaro.tanba <hnnskrhtnbsd2600@icloud.com>
2020-09-27 21:48:08 +0900

    Initial Commit

---
+++ slideshowappp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideshowappp
+//
+//  Created by HOUkoku on 2020/09/27.
+//  Copyright © 2020 jintaro.tanba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
